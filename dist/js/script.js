29151c3bac3c9a40a65af87c82496a94fdb68add
Nurul Mukhlisa <numulaa@gmail.com>
2023-02-14 20:14:59 +0800

    did the carousel and modify the project section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,23 @@
-const btn = document.getElementById("menu-btn");
-const nav = document.getElementById("menu");
+// const btn = document.getElementById("menu-btn");
+// const nav = document.getElementById("menu");
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
-  nav.classList.toggle("flex");
-  nav.classList.toggle("hidden");
+// btn.addEventListener("click", () => {
+//   btn.classList.toggle("open");
+//   nav.classList.toggle("flex");
+//   nav.classList.toggle("hidden");
+// });
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "prev" ? -1 : 1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         darkChoco: "#514833",
         white: "#FFFFFF",
         gray: "#B9B6B6",
+        bgGray: "rgba(225, 225, 225, .7)",
         lightGray: "#D1D1D1",
         textChoco: "rgba(35, 29, 25, 0.7)",
         textGrayish: "rgba(35, 29, 25, 0.1)",
